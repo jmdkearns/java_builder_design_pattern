b341654b4f9a691cdf8ee553b28c77e42749d2ab
Josh Kearns <jmdkearns@gmail.com>
2016-08-11 09:36:02 +0100

    attempted..

---
+++ BMW.java
@@ -0,0 +1,11 @@
+public class BMW implements Car {
+
+  @Override
+  public String manufacturer() {
+    return "BMW";
+  }
+
+  @Override
+  public int 
+
+}
---
+++ Car.java
@@ -0,0 +1,5 @@
+public interface Car {
+  public String manufacturer();
+  public int engineSize();
+  public String addOn();
+}
